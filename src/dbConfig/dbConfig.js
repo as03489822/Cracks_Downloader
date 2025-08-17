d917b1c7272f1d7c55e9683c56203815d240782f
as03489822 <as03489822@gmail.com>
2025-08-17 23:26:26 +0500

    add mongodb atlas url and vercel.json

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,18 +1,22 @@
-import mongoose from 'mongoose';
+// lib/dbConnect.js
+import mongoose from "mongoose";
 
-export async function connectDB(){
-    try{
-        mongoose.connect(process.env.MONGO_URL);
-        const connection = mongoose.connection;
-        connection.on('connected' , ()=>{
-            console.log("Mongo DB connected")
-        })
-        connection.on('error' , (err)=>{
-            console.log("Mongo DB connection error, please make sure DB in up  and runnning" + err);
-            process.exit()
-        })
-    }catch(error){
-        console.log("something went wrong in connecting to  DB")
-        console.log(error)
-    }
-}
+let isConnected = false; // track connection
+
+export const connectDB = async () => {
+  if (isConnected) {
+    console.log("✅ MongoDB already connected");
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect("mongodb+srv://crack:crack@cluster0.le0hn.mongodb.net/crack?retryWrites=true&w=majority&appName=Cluster0", {
+    });
+
+    isConnected = conn.connections[0].readyState === 1;
+    console.log("🚀 MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    throw new Error("MongoDB connection failed");
+  }
+};
